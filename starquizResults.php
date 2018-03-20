aca93e7f83c82726bc5f139e2222f95df8579b3e
Gary Hsieh <garyhsieh@gmail.com>
2018-03-20 13:48:59 -0700

    Setting up StarWars Quiz

---
+++ starquizResults.php
@@ -0,0 +1,56 @@
+<?php
+echo "First Name: ".$_POST['Q1'];
+?>
+
+</br>
+
+<?php
+echo "First Name: ".$_POST['Q2'];
+?>
+
+</br>
+
+<?php
+echo "First Name: ".$_POST['Q3'];
+?>
+
+</br>
+
+<?php
+echo "First Name: ".$_POST['Q4'];
+?>
+
+<?php
+echo "First Name: ".$_POST['Q5'];
+?>
+
+</br>
+
+<?php
+echo "First Name: ".$_POST['Q6'];
+?>
+
+</br>
+
+<?php
+echo "First Name: ".$_POST['Q7'];
+?>
+
+</br>
+
+<?php
+echo "First Name: ".$_POST['Q8'];
+?>
+
+<?php
+echo "First Name: ".$_POST['Q9'];
+?>
+
+</br>
+
+<?php
+echo "First Name: ".$_POST['Q10'];
+?>
+
+
+</br>
