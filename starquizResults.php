6772042c6cc9ec26213892ca809b0991b417324e
lucascolusso <lucascolusso@gmail.com>
2018-03-23 12:58:51 -0700

    images

--- starquizResults.php
+++ starquizResults.php
@@ -9,9 +9,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
-$sql = "INSERT INTO responses (user_ip, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10) 
+$sql = "INSERT INTO responses (user_ip, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10)
 VALUES ('$_SERVER[REMOTE_ADDR]','$_POST[Q1]','$_POST[Q2]','$_POST[Q3]','$_POST[Q4]','$_POST[Q5]','$_POST[Q6]','$_POST[Q7]','$_POST[Q8]','$_POST[Q9]','$_POST[Q10]');";
 
 
@@ -82,7 +82,7 @@ function shuffle_assoc($array)
 
         $shuffled_array[  $shuffled_key  ] = $array[  $shuffled_key  ];
 
-    } 
+    }
 
 
     return $shuffled_array;
@@ -104,5 +104,3 @@ header('Location: http://buzz.hcde.uw.edu/starquiz_'.$redirect_url.'.html');
 die();
 
 ?>
-
-
